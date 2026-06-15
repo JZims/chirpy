3d2fc67ab1114e82dcbd508633cdbe18ad28d8e0
JZims <zimmerman.j@hotmail.com>
2026-06-15 17:05:02 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JZims/chirpy
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	handler := http.NewServeMux()
+
+	server := http.Server{
+		Handler: handler,
+		Addr:    ".8080",
+	}
+
+	server.ListenAndServe()
+
+}
