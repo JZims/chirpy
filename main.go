76d548e8b61d6ebe330e228cb55054a0de850830
JZims <zimmerman.j@hotmail.com>
2026-06-18 19:23:11 -0400

    added handler for server health checker

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	port := "8080"
-	handler := http.NewServeMux()
+	mux := http.NewServeMux()
 
-	server := http.Server{
-		Handler: handler,
+	server := &http.Server{
+		Handler: mux,
 		Addr:    ":" + port,
 	}
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	log.Printf("Server started on port: %v", server.Addr)
 	log.Fatal(server.ListenAndServe())
